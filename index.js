15082340a8568695c107fe2573eb834fabf339c4
Makoto <ninjmiley@gmail.com>
2025-04-17 12:52:45 -0300

    re:try

--- index.js
+++ index.js
@@ -11,24 +11,47 @@ app.get("/moves/:pokemon", async (req, res) => {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         const moves = response.data.moves;
 
-        const levelUpMoves = moves
-            .filter(move =>
-                move.version_group_details.some(detail =>
-                    detail.move_learn_method.name === "level-up"
-                )
-            )
-            .map(move => move.move.name) // mantém hífens
-            .filter((value, index, self) => self.indexOf(value) === index) // remove duplicatas
-            .sort();
+        const levelUpMoves = [];
+
+        moves.forEach(move => {
+            move.version_group_details.forEach(detail => {
+                if (detail.move_learn_method.name === "level-up") {
+                    levelUpMoves.push({
+                        name: move.move.name,
+                        level: detail.level_learned_at
+                    });
+                }
+            });
+        });
+
+        // Remove duplicatas mantendo o menor nível aprendido
+        const uniqueMoves = {};
+        levelUpMoves.forEach(move => {
+            if (!uniqueMoves[move.name] || move.level < uniqueMoves[move.name]) {
+                uniqueMoves[move.name] = move.level;
+            }
+        });
+
+        // Ordena por nível e formata como "Move|Level|Move|Level|..."
+        const sortedMoves = Object.entries(uniqueMoves)
+            .sort((a, b) => a[1] - b[1])
+            .map(([name, level]) => `${capitalize(name)}|${level}`); // Com inicial maiúscula
+
+        const finalString = [sortedMoves.join("|")];
 
         res.json({
             pokemon: pokemonName,
-            levelUpMoves
+            levelUpMoves: finalString
         });
+
     } catch (error) {
         res.status(404).json({ error: "Pokémon não encontrado ou erro ao buscar dados." });
     }
 });
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 app.listen(PORT, () => {
     console.log(`API rodando em http://localhost:${PORT}`);
